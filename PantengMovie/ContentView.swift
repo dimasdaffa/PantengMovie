7b58f8b80a4b3a57651c1751ee880cc3ee2b3b65
dimasdaffa <dimas.de2004@gmail.com>
2026-03-06 08:49:49 +0700

    Initial Commit

---
+++ PantengMovie/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PantengMovie
+//
+//  Created by DIMAS DAFFA ERNANDA on 06/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView{
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PantengMovie/PantengMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantengMovieApp.swift
+//  PantengMovie
+//
+//  Created by DIMAS DAFFA ERNANDA on 06/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PantengMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
