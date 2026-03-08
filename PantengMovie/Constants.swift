2e529d30c4706004d2141250f702d084686153b3
dimasdaffa <dimas.de2004@gmail.com>
2026-03-08 12:27:47 +0700

    feat: Implement core UI structure with tab navigation, home screen layout, horizontal content lists, and centralized constants.

--- PantengMovie/Constants.swift
+++ PantengMovie/Constants.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Constants {
+    static let homeString = "Home"
+    static let upcomingString = "Upcoming"
+    static let searchString = "Search"
+    static let downloadString = "Download"
+    static let playString = "Play"
+    static let trendingMovieString = "Trending Movies"
+    static let trendingTVString = "Trending TV"
+    static let topRatedMovieString = "Top Rated Movies"
+    static let topRatedTVString = "Top Rated TV"   
+    
+    static let homeIconString = "house"
+    static let upcomingIconString = "play.circle"
+    static let searchIconString = "magnifyingglass"
+    static let downloadIconString = "arrow.down.to.line"
+    
+    static let testTitleURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8qtCT7IlW9fmgySlBrDPiGl3iSC5zxM1VlA&s"
+    static let testTitleURL2 = "https://nos.jkt-1.neo.id/media.cinema21.co.id/movie-images/25F1XX.jpg"
+    static let testTitleURL3 = "https://upload.wikimedia.org/wikipedia/id/9/98/Our_Movie_poster.png"
+}
+
+extension Text {
+    func ghostButton() -> some View {
+        self
+            .frame(width: 100, height: 50)
+            .foregroundStyle(.buttonText)
+            .bold()
+            .background {
+                RoundedRectangle(cornerRadius: 20,style: .continuous)
+                    .stroke(.buttonBorder,lineWidth: 5)
+            }
+    }
+} 
+ 
--- PantengMovie/ContentView.swift
+++ PantengMovie/ContentView.swift
@@ -10,12 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView{
-            Divider()
-            
-        }
+            Tab(Constants.homeString,systemImage: Constants.homeIconString){
+                HomeView()
+            }
+            Tab(Constants.upcomingString,systemImage:   Constants.upcomingIconString){
+                Text(Constants.upcomingString)
+            }
+            Tab(Constants.searchString,systemImage: Constants.searchIconString){
+                Text(Constants.searchString)
+            }
+            Tab(Constants.downloadString,systemImage: Constants.downloadIconString){
+                Text(Constants.downloadString)
+            }
+                
+                }
     }
 }
 
 #Preview {
     ContentView()
 }
+  
