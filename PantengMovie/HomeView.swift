94bf483b2097d4b367d889a70f9a7ca1fad095ad
dimasdaffa <dimas.de2004@gmail.com>
2026-03-08 12:27:32 +0700

    feat: Implement initial HomeView, HorizontalListView, tab bar navigation, and supporting UI constants and color assets.

---
+++ PantengMovie/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  PantengMovie
+//
+//  Created by DIMAS DAFFA ERNANDA on 07/03/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ PantengMovie/HorizontalListView.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalListView.swift
+//  PantengMovie
+//
+//  Created by DIMAS DAFFA ERNANDA on 08/03/26.
+//
+
+import SwiftUI
+
+struct HorizontalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HorizontalListView()
+}
